9c1582a8be2147db025b135d90b50987e286b604
HeegyeongJ <gmlrud0409@gmail.com>
2024-01-13 06:09:23 +0900

    로딩 로직 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import FirstProgram from "./pages/Programs/FirstProgram";
 import SecondProgram from "./pages/Programs/SecondProgram";
 import BackCover from "./pages/BackCover/BackCover";
 import { useMediaQuery } from "react-responsive";
-import { useEffect, useState } from "react";
-import WelcomLoading from "./pages/WelcomeLoading/WelcomLoading";
 
 function App() {
   const isMobile = useMediaQuery({
@@ -18,35 +16,26 @@ function App() {
     anchors: ["main", "musicians", "program1", "program2", "backCover"],
   };
 
-  const [main, setMain] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setMain(true), 3000);
-  }, []);
   return (
     <>
       {isMobile ? (
-        !main ? (
-          <WelcomLoading />
-        ) : (
-          <SectionsContainer {...options}>
-            <Section>
-              <Main />
-            </Section>
-            <Section>
-              <Musicians />
-            </Section>
-            <Section>
-              <FirstProgram />
-            </Section>
-            <Section>
-              <SecondProgram />
-            </Section>
-            <Section>
-              <BackCover />
-            </Section>
-          </SectionsContainer>
-        )
+        <SectionsContainer {...options}>
+          <Section>
+            <Main />
+          </Section>
+          <Section>
+            <Musicians />
+          </Section>
+          <Section>
+            <FirstProgram />
+          </Section>
+          <Section>
+            <SecondProgram />
+          </Section>
+          <Section>
+            <BackCover />
+          </Section>
+        </SectionsContainer>
       ) : (
         <div className="pc">모바일 환경에서만 보실 수 있습니다.</div>
       )}
