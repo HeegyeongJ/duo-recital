5a6d900cd7ee619b699d6b4b9bc62925ab3fa09f
HeegyeongJ <gmlrud0409@gmail.com>
2024-01-26 20:08:55 +0900

    스크롤 표시 삽입

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Main = () => {
-  return <img className="main" src="/assets/Dancing_of_Flowers.png" alt="" />;
+  return (
+    <div className="main">
+      <img className="main_img" src="/assets/Dancing_of_Flowers.png" alt="" />
+      <div className="arrow_box">
+        <img className="down_arrow" src="/assets/down_arrow.svg" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
